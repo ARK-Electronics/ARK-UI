a681101c76354fd4d048400f974c75949b48a2e6
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-06-20 23:20:33 -0800

    source nvm

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ sudo apt-get install -y jq nodejs npm nginx
 sudo npm install -g @vue/cli @vue/cli-service@latest
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 20
 cd backend
 npm install
