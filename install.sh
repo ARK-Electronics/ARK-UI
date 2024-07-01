140680ea96c2cebd7c2774f3e0c2fb443e286854
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-07-01 13:06:30 -0800

    attempt to fix install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,33 @@ pushd .
 
 cd "$SCRIPT_DIR"
 
-sudo apt-get install -y jq nodejs npm nginx
-sudo npm install -g @vue/cli @vue/cli-service@latest
+sudo apt-get update
+# sudo apt-get install -y jq nodejs npm nginx
+sudo apt-get install -y curl jq nginx
+# sudo npm install -g @vue/cli @vue/cli-service@latest
+
+# Install NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Source the NVM scripts to use it in the same script
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+
+# Install the desired Node.js version and set it as default
 nvm install 20
+nvm use 20
+nvm alias default 20
+
+# Install global Vue CLI
+npm install -g @vue/cli @vue/cli-service@latest
+
+# Install dependencies and build the project in backend directory
 cd backend
 npm install
+
+# Install dependencies and build the project in ark-ui directory
 cd ../ark-ui
 npm install
 npm run build
