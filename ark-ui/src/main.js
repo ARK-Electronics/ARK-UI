1fb37cb5769fbf93f84b5e2f747f75e5b1c0088b
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-10-10 13:29:04 -0800

    added fontawesome to package.json instead of CDN

--- ark-ui/src/main.js
+++ ark-ui/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createApp(App).use(router).mount('#app');
